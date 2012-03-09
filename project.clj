baec6b369814c96ec4777f1fc625664df061d77f
Theo Pack <tf.pack@googlemail.com>
2012-03-09 23:39:00 +0100

    update deps

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject xmpp-repl "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :description "XMPP CLient for the repl"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [jivesoftware/smack "3.1.0"]])
