8d34edac66ebfd5b41c94dfd05c777c121805870
Theo Pack <tf.pack@googlemail.com>
2012-03-09 22:58:01 +0100

    project files

---
+++ src/xmpp_repl/core.clj
@@ -0,0 +1,1 @@
+(ns xmpp-repl.core)
---
+++ test/xmpp_repl/test/core.clj
@@ -0,0 +1,6 @@
+(ns xmpp-repl.test.core
+  (:use [xmpp-repl.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
