3a9891a94a7d40c2c9863e2a95def1f7e8684807
Theo Pack <tf.pack@googlemail.com>
2012-03-10 17:02:04 +0100

    create login function

--- src/xmpp_repl/core.clj
+++ src/xmpp_repl/core.clj
@@ -1,1 +1,11 @@
 (ns xmpp-repl.core)
+
+(def connections (agent {}))
+
+(defn login [email password]
+  (let [[user server] (.split email "@")
+        connection (doto (org.jivesoftware.smack.XMPPConnection. server)
+                     (.connect)
+                     (.login user password))]
+    (doseq [x (.getEntries (.getRoster connection))]
+      (println x))))
