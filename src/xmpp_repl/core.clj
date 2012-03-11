5525bff74a5cf83e8bfa301a783447f5e7b750ec
Theo Pack <tf.pack@googlemail.com>
2012-03-11 18:28:06 +0100

    refactor

--- src/xmpp_repl/core.clj
+++ src/xmpp_repl/core.clj
@@ -4,6 +4,9 @@
 
 (def connections (agent {}))
 
+(defn connections-as-list []
+  (map #(:connection (nth %1 1)) @connections))
+
 (defmulti receive-msg (fn [from _] from))
 (defmethod receive-msg :default [from msg] (println "Message from" from ":" msg))
 
@@ -26,24 +29,12 @@
     (regigster-listener connection)
     (send connections conj {email {:connection connection}})))
 
-(defn send-message [to msg]
-  (let [packet (doto (Message.)
-                 (.setTo to)
-                 (.setBody msg))]
-    (-> (contact-connection to) (.sendPacket packet))))
-
-(defn logout [email]
-  (send connections dissoc email))
-
 (defn contacts [connection]
   (-> connection .getRoster .getEntries))
 
 (defn contains-contact [connection email]
   (some #(= email %1)
         (map #(.getUser %1) (contacts connection))))
-
-(defn connections-as-list []
-  (map #(:connection (nth %1 1)) @connections))
 
 (defn contact-connection [email]
   (let [av-cons (filter
@@ -53,11 +44,19 @@
       (first (connections-as-list))
       (first av-cons))))
 
+(defn send-message [to msg]
+  (let [packet (doto (Message.)
+                 (.setTo to)
+                 (.setBody msg))]
+    (-> (contact-connection to) (.sendPacket packet))))
+
+(defn logout [email]
+  (send connections dissoc email))
+
 (defn show-contacts []
   (doseq [connection (connections-as-list)]
     (doseq [client (contacts connection)]
-      (println client)
-      (println (-> connection .getRoster (.getPresence (.getUser client)))))))
+      (println client "->" (-> connection .getRoster (.getPresence (.getUser client)))))))
 
 (defmacro add-fav
   ([contact] (let [msg (gensym)
